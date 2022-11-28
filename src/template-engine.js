bdd86d4ed36d6bd1344991103f77c18f047321fb
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2022-11-28 03:04:22 +0100

    initialized and configured code formatter and analyzer, fixed found issues with js

--- src/template-engine.js
+++ src/template-engine.js
@@ -14,17 +14,22 @@
     ]
 }
 */
+// eslint-disable-next-line no-unused-vars
 function templateEngine(block) {
     if (block === undefined || block === null || block === false) {
         return document.createTextNode('');
     }
-    if (typeof block === 'string' || typeof block === 'number' || block === true) {
+    if (
+        typeof block === 'string' ||
+        typeof block === 'number' ||
+        block === true
+    ) {
         return document.createTextNode(block);
     }
     if (Array.isArray(block)) {
         const fragment = document.createDocumentFragment();
 
-        block.forEach(element => {
+        block.forEach((element) => {
             fragment.appendChild(templateEngine(element));
         });
 
@@ -35,7 +40,7 @@ function templateEngine(block) {
 
     if (block.cls) {
         const classes = [].concat(block.cls);
-        classes.forEach(cls => {
+        classes.forEach((cls) => {
             result.classList.add(cls);
         });
     }
@@ -43,7 +48,7 @@ function templateEngine(block) {
     if (block.attrs) {
         const keys = Object.keys(block.attrs);
 
-        keys.forEach(key => {
+        keys.forEach((key) => {
             result.setAttribute(key, block.attrs[key]);
         });
     }
